2f89d232e261263bcc1782a9b2498d6afdedb9b8
IamPis <kisatomichael@gmail.com>
2022-09-08 19:46:11 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 /*
- *Comment
+ *main - entry or start point
+ *
+ *Return: 0 if successful
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-i}
+}
