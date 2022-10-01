ea916b6d0e461381a628ceee5edabcc38080bb8b
IamPis <kisatomichael@gmail.com>
2022-10-01 12:08:41 -0700

    what you answer to

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "stdio.h"
+
+/**
+ * main - print the program name
+ * @argc: number of arguements
+ * @argv: array of pointers
+ * Return: always 0
+ */
+int main(int __attribute__((unused))argc, char *argv[])
+{
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
